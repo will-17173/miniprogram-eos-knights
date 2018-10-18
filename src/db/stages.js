1ab72ab8c26cd6723859751278b9beb2447c974c
Will.H <huangzhaojian@gmail.com>
2018-10-18 14:48:17 +0800

    移动stages

---
+++ src/db/stages.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 0,
+    dropRate: .22,
+    sortRates: [.3, .3, .1, .2, .1]
+  },
+  {
+    id: 1,
+    dropRate: .22,
+    sortRates: [.3, .3, .1, .2, .1]
+  },
+  {
+    id: 2,
+    dropRate: .22,
+    sortRates: [.3, .3, .1, .2, .1]
+  },
+  {
+    id: 3,
+    dropRate: .22,
+    sortRates: [.3, .3, .1, .2, .1]
+  },
+  {
+    id: 4,
+    dropRate: .22,
+    sortRates: [.3, .3, .1, .2, .1]
+  }
+]
